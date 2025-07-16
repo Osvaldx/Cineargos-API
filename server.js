6354949fd9d9f860f25d45dfd4be7155e40dc260
osvaldx <romanonahuel24@gmail.com>
2025-07-16 17:08:28 -0300

    the basic server was configured and the auth route was added.

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-// ...
+import express from "express";
+import cors from "express";
+import environments from "./src/api/config/environments.js";
+import { AuthRouter } from "./src/api/routes/index.js";
+
+const app = express();
+const PORT = environments.port;
+
+app.use(cors()); // Para permitir peticiones
+app.use(express.json()); // Para recibir datos del body
+
+app.use("/api", AuthRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server corriendo en: http://localhost:${PORT}/`);
+})
