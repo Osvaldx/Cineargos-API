9ca75e1bc53172cea8f8d93043ec5482def59d39
osvaldx <romanonahuel24@gmail.com>
2025-07-16 17:13:23 -0300

    the post method is handled with its middleware and controllers.

--- src/api/routes/auth.router.js
+++ src/api/routes/auth.router.js
@@ -1,1 +1,9 @@
-// ...
+import { Router } from "express";
+import authController from "../controllers/authController.js";
+import middlewares from "../middlewares/middlewares.js";
+
+const router = Router();
+
+router.post("/auth",middlewares.validateAuthParameters, authController.login)
+
+export default router;
