308f12c01e6465fa8e7e29c14031529780a8a01a
osvaldx <romanonahuel24@gmail.com>
2025-07-16 17:10:51 -0300

    the query to the database was placed

--- src/api/models/auth.models.js
+++ src/api/models/auth.models.js
@@ -1,1 +1,11 @@
-// ...
+import connection from "../database/db.js";
+
+const autenticationDB = async(email, password) => {
+    let sql = "SELECT * FROM users WHERE (email=?)"
+
+    return await connection.query(sql, [email,password]);
+}
+
+export default {
+    autenticationDB
+}
