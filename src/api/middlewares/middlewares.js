908cdbafec564de0c7b4bc850a5ee66a21314485
osvaldx <romanonahuel24@gmail.com>
2025-07-16 17:12:33 -0300

    jwt token and parameter validations were created

--- src/api/middlewares/middlewares.js
+++ src/api/middlewares/middlewares.js
@@ -1,1 +1,39 @@
-// ...
+const validateToken = async (req, res, next) => {
+    const token = req.headers["authorization"];
+
+    if(!token) {
+        res.status(401).json({
+            message: "You don´t have access or the token is expired"
+        })
+    } else {
+        next();
+    }
+}
+
+const validateAuthParameters = async (req, res, next) => {
+    try {
+        const regExCorreo = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
+        const { email, password } = req.body;
+    
+        if(!email || !password) {
+            return res.status(401).json({
+                message: "[!] Invalid credentials"
+            });
+        }
+
+        if(!regExCorreo.test(email)) {
+            return res.status(401).json({
+                message: "[!] Invalid Email"
+            });
+        }
+
+        next();
+    } catch(error) {
+        console.log(`[!] ERROR: ${error}`);
+    }
+}
+
+export default {
+    validateToken,
+    validateAuthParameters
+}
