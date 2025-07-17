f6a7864dd993a7e8b5293796f74e460bb2fbc15a
osvaldx <romanonahuel24@gmail.com>
2025-07-17 11:16:22 -0300

    added new registerDB function with sql query

--- src/api/models/auth.models.js
+++ src/api/models/auth.models.js
@@ -1,11 +1,18 @@
 import connection from "../database/db.js";
 
-const autenticationDB = async(email, password) => {
-    let sql = "SELECT * FROM users WHERE (email=?)"
+const autenticationDB = async(email) => {
+    let sql = "SELECT * FROM users WHERE (email=?)";
 
-    return await connection.query(sql, [email,password]);
+    return await connection.query(sql, [email]);
+}
+
+const registerDB = async (firstName, email, password) => {
+    let sql = "INSERT INTO users (firstName, email, password) VALUES (?,?,?)";
+
+    return await connection.query(sql, [firstName,email,password]);
 }
 
 export default {
-    autenticationDB
+    autenticationDB,
+    registerDB
 }
