9dee51404dacb12318d89762e77e56ad18b486b0
osvaldx <romanonahuel24@gmail.com>
2025-07-17 11:19:31 -0300

    added register and fix some type error

--- src/api/controllers/authController.js
+++ src/api/controllers/authController.js
@@ -7,19 +7,19 @@ const login = async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        let [rows] = await Auth.autenticationDB(email, password);
+        let [rows] = await Auth.autenticationDB(email);
 
         if(rows.length === 0) {
             return res.status(401).json({
                 message: "[!] Invalid credentials"
             });
         }
-
+        
         const isMatch = await bcrypt.compare(password, rows[0].password);
 
         if(!isMatch) {
             return res.status(401).json({
-                message: "[!] Invalid credentials"
+                message: "[!] Invalid password"
             });
         }
         
@@ -33,10 +33,45 @@ const login = async (req, res) => {
         console.log(`[!] ERROR: ${error}`)
         res.status(500).json({
             message: `[!] ERROR INTERNO DEL SERVIDOR`
-        })
+        });
+    }
+}
+
+const register = async (req, res) => {
+    try {
+        const { firstName, email, password } = req.body;
+
+        const saltRounds = 10;
+        const passwordEncrypted = await bcrypt.hash(password, saltRounds);
+        
+        let rows;
+        try {
+            [rows] = await Auth.registerDB(firstName,email,passwordEncrypted);
+        } catch(error) {
+            return res.status(409).json({
+                message: "[!] Ya existe una cuenta con ese email"
+            })
+        }
+
+        if(rows.affectedRows > 0) {
+            return res.status(200).json({
+                message: "[+] Cuenta registrada con exito!"
+            });
+        } else {
+            return res.status(500).json({
+                message: "[!] No se pudo registrar la cuenta. Intente nuevamente."
+            });
+        }
+
+    } catch(error) {
+        console.log(`[!] ERROR: ${error}`)
+        res.status(500).json({
+            message: `[!] ERROR INTERNO DEL SERVIDOR`
+        });
     }
 }
 
 export default {
-    login
+    login,
+    register
 }
