0f696530df8c989cb5b13abe9c5a3409b7609f99
osvaldx <romanonahuel24@gmail.com>
2025-07-23 14:17:58 -0300

    add the logic of getMovieID

--- src/api/controllers/moviesController.js
+++ src/api/controllers/moviesController.js
@@ -22,3 +22,27 @@ export const getAllMovies = async(req, res) => {
         });
     }
 }
+
+export const getMovieID = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const [rows] = await moviesDB.getMovieIDDB(id);
+
+        if(rows.length < 1) {
+            return res.status(404).json({
+                message: "[!] NO SE ENCONTRO LA PELICULA CON ESE ID"
+            });
+        }
+
+        return res.status(200).json({
+            message: "[+] PELICULA ENCONTRADA!",
+            payload: rows
+        });
+
+    } catch (error) {
+        console.log(`[!] MOVIES: ${error}`)
+        return res.status(500).json({
+            message: "[!] ERROR INTERNO DEL SERVIDOR"
+        });
+    }
+}
