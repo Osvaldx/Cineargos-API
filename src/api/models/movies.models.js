accfdb0863a6186088d312da2b56e0d359ad192e
osvaldx <romanonahuel24@gmail.com>
2025-07-23 13:56:23 -0300

    add DB queries

---
+++ src/api/models/movies.models.js
@@ -0,0 +1,39 @@
+import connection from "../database/db.js";
+
+const getAllMoviesDB = () => {
+    let sql = "SELECT * FROM movies;";
+
+    return connection.query(sql);
+}
+
+const getMovieIDDB = (id) => {
+    let sql = "SELECT * FROM movies WHERE (id_movie = ?)";
+
+    return connection.query(sql, [id]);
+}
+
+const addMovieDB = (title, image, isAvailable) => {
+    let sql = "INSERT INTO movies (title, image, isAvailable) VALUES (?,?,?);"
+
+    return connection.query(sql, [title,image,isAvailable]);
+}
+
+const modifiedMovieDB = (id, title, genre, releaseYear, director, image, isAvailable) => {
+    let sql = "UPDATE movies SET title = ?, genre = ?, releaseYear = ?, director = ?, image = ?, isAvailable = ? WHERE (id_movie = ?);";
+
+    return connection.query(sql, [title,genre,releaseYear,director,image,isAvailable,id]);
+}
+
+const deleteMovieDB = (id) => {
+    let sql = "DELETE FROM movies WHERE (id_movie = ?);";
+
+    return connection.query(sql, [id]);
+}
+
+export default {
+    getAllMoviesDB,
+    getMovieIDDB,
+    addMovieDB,
+    modifiedMovieDB,
+    deleteMovieDB
+}
