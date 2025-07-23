f102d37f9502b36b2dd5be334c68689c2f6e9a40
osvaldx <romanonahuel24@gmail.com>
2025-07-23 13:55:45 -0300

    add morgan and moviesrouter

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 import express from "express";
 import cors from "express";
 import environments from "./src/api/config/environments.js";
-import { AuthRouter } from "./src/api/routes/index.js";
+import morgan from "morgan";
+import { AuthRouter, MoviesRouter } from "./src/api/routes/index.js";
 
 const app = express();
 const PORT = environments.port;
 
 app.use(cors()); // Para permitir peticiones
 app.use(express.json()); // Para recibir datos del body
+app.use(morgan("dev"));
 
 app.use("/api", AuthRouter);
+app.use("/api", MoviesRouter);
 
 app.listen(PORT, () => {
     console.log(`Server corriendo en: http://localhost:${PORT}/`);
