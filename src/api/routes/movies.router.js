8ae0217ffe60b73dfb5bd4b08365e9b2e80eec01
osvaldx <romanonahuel24@gmail.com>
2025-07-23 22:10:32 -0300

    add the middleware isAdmin

--- src/api/routes/movies.router.js
+++ src/api/routes/movies.router.js
@@ -6,7 +6,7 @@ const router = Router();
 
 router.get("/movies", getAllMovies);
 router.get("/movies/:id", middlewares.validateMovieID, getMovieID);
-router.post("/movies/add", middlewares.validateToken, middlewares.validateMovieParameters, addMovie);
+router.post("/movies/add", middlewares.validateToken, middlewares.isAdmin, middlewares.validateMovieParameters, addMovie);
 // router.put("/movies/update/:id",middlewares.validateMovieID,middlewares.validateMovieParameters,);
 // router.delete("/movies/delete/:id", middlewares.validateMovieID,);
 
