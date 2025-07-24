1326347d45e2b7977e819116b591e1cd0df522cd
osvaldx <romanonahuel24@gmail.com>
2025-07-23 22:02:22 -0300

    add the POST method of addMovie

--- src/api/routes/movies.router.js
+++ src/api/routes/movies.router.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import middlewares from "../middlewares/middlewares.js";
-import { getAllMovies, getMovieID } from "../controllers/moviesController.js";
+import { getAllMovies, getMovieID, addMovie } from "../controllers/moviesController.js";
 
 const router = Router();
 
 router.get("/movies", getAllMovies);
 router.get("/movies/:id", middlewares.validateMovieID, getMovieID);
-// router.post("/movies/add", middlewares.validateMovieParameters,);
+router.post("/movies/add", middlewares.validateToken, middlewares.validateMovieParameters, addMovie);
 // router.put("/movies/update/:id",middlewares.validateMovieID,middlewares.validateMovieParameters,);
 // router.delete("/movies/delete/:id", middlewares.validateMovieID,);
 
