c6ed45fc8fdb49e8854f6b2f4cd06b755f6fca6d
osvaldx <romanonahuel24@gmail.com>
2025-08-20 20:22:59 -0300

    add the route of deleteMovie and middlewares

--- src/api/routes/movies.router.js
+++ src/api/routes/movies.router.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import middlewares from "../middlewares/middlewares.js";
-import { getAllMovies, getMovieID, addMovie, updateMovie } from "../controllers/moviesController.js";
+import { getAllMovies, getMovieID, addMovie, updateMovie, deleteMovie } from "../controllers/moviesController.js";
 
 const router = Router();
 
@@ -8,6 +8,6 @@ router.get("/movies", getAllMovies);
 router.get("/movies/:id", middlewares.validateMovieID, getMovieID);
 router.post("/movies/add", middlewares.validateToken, middlewares.isAdmin, middlewares.validateMovieParameters, addMovie);
 router.put("/movies/update/:id", middlewares.validateToken, middlewares.isAdmin, middlewares.validateMovieID, middlewares.validateMovieParameters, updateMovie);
-// router.delete("/movies/delete/:id", middlewares.validateMovieID,);
+router.delete("/movies/delete/:id", middlewares.validateToken, middlewares.isAdmin, middlewares.validateMovieID, deleteMovie);
 
 export default router;
