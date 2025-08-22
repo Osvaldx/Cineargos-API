782554663cfc28884ec12083b29e65252037a88c
osvaldx <romanonahuel24@gmail.com>
2025-08-22 01:17:02 -0300

    fix type

--- src/api/models/auth.models.js
+++ src/api/models/auth.models.js
@@ -2,13 +2,11 @@ import connection from "../database/db.js";
 
 const autenticationDB = async(email) => {
     let sql = "SELECT * FROM users WHERE (email=?)";
-
     return await connection.query(sql, [email]);
 }
 
 const registerDB = async (firstName, email, password) => {
     let sql = "INSERT INTO users (firstName, email, password) VALUES (?,?,?)";
-
     return await connection.query(sql, [firstName,email,password]);
 }
 
--- src/api/models/movies.models.js
+++ src/api/models/movies.models.js
@@ -2,31 +2,26 @@ import connection from "../database/db.js";
 
 const getAllMoviesDB = () => {
     let sql = "SELECT * FROM movies;";
-
     return connection.query(sql);
 }
 
 const getMovieIDDB = (id) => {
     let sql = "SELECT * FROM movies WHERE (id_movie = ?)";
-
     return connection.query(sql, [id]);
 }
 
 const addMovieDB = (title, genre, releaseYear, director, image, isAvailable) => {
     let sql = "INSERT INTO movies (title, genre, releaseYear, director, image, isAvailable) VALUES (?,?,?,?,?,?);"
-
     return connection.query(sql, [title,genre,releaseYear,director,image,isAvailable]);
 }
 
 const modifiedMovieDB = (id, title, genre, releaseYear, director, image, isAvailable) => {
     let sql = "UPDATE movies SET title = ?, genre = ?, releaseYear = ?, director = ?, image = ?, isAvailable = ? WHERE (id_movie = ?);";
-
     return connection.query(sql, [title,genre,releaseYear,director,image,isAvailable,id]);
 }
 
 const deleteMovieDB = (id) => {
     let sql = "DELETE FROM movies WHERE (id_movie = ?);";
-
     return connection.query(sql, [id]);
 }
 
