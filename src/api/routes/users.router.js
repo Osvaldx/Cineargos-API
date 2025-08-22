a274c91ae5a4a2def46b5d16f74952e68068c448
osvaldx <romanonahuel24@gmail.com>
2025-08-21 21:42:24 -0300

    add the call for controllers

--- src/api/routes/users.router.js
+++ src/api/routes/users.router.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import middlewares from "../middlewares/middlewares";
+import { getAllUsers, getUser, addUser, updateUser, deletUser } from "../controllers/usersController.js";
 
 const router = Router();
 
-router.get("/users", middlewares.validateToken, middlewares.isAdmin,);
-router.get("/users/:id", middlewares.validateToken, middlewares.isAdmin, middlewares.validateID,);
-router.post("/users/add", middlewares.validateToken, middlewares.isAdmin, "crear middlewares para validar users body",);
-router.put("/users/update/:id", middlewares.validateToken, middlewares.isAdmin, middlewares.validateID, "crear middlewares para validar body users",);
-router.delete("/users/delete/:id", middlewares.validateToken, middlewares.validateID,);
+router.get("/users", middlewares.validateToken, middlewares.isAdmin, getAllUsers);
+router.get("/users/:id", middlewares.validateToken, middlewares.isAdmin, middlewares.validateID, getUser);
+router.post("/users/add", middlewares.validateToken, middlewares.isAdmin, "crear middlewares para validar users body", addUser);
+router.put("/users/update/:id", middlewares.validateToken, middlewares.isAdmin, middlewares.validateID, "crear middlewares para validar body users", updateUser);
+router.delete("/users/delete/:id", middlewares.validateToken, middlewares.validateID, deletUser);
 
 export default router;
