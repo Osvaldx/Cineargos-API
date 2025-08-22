9167cd3f5145ae1940e99a1345bda26937ac9df8
osvaldx <romanonahuel24@gmail.com>
2025-08-22 01:15:09 -0300

    add UsersRouter in the barril

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,7 +1,9 @@
 import AuthRouter from "./auth.router.js";
 import MoviesRouter from "./movies.router.js";
+import UsersRouter from "./users.router.js";
 
 export {
     AuthRouter,
-    MoviesRouter
+    MoviesRouter,
+    UsersRouter
 }
